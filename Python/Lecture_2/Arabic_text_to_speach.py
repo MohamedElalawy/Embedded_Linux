3fa3f75f39fa6a9bd0aa1a5d3b0d6e47260afc2b
Elalawy <74903353+MohamedElalawy@users.noreply.github.com>
2025-07-07 23:11:14 +0300

    Update Arabic_text_to_speach.py

--- Python/Lecture_2/Arabic_text_to_speach.py
+++ Python/Lecture_2/Arabic_text_to_speach.py
@@ -1,13 +1,11 @@
 from gtts import gTTS
 import os
 
-arabic_text = "أجدع علوي على الكوكب."
+arabic_text = "هو فيه جييييييييييييه."
 
 tts = gTTS(text=arabic_text, lang='ar')
 tts.save("arabic_speech.mp3")
 
 print("Done! Saved as arabic_speech.mp3")
-
-# Play the audio (Linux example using 'mpg123')
 os.system("mpg123 arabic_speech.mp3")
 
